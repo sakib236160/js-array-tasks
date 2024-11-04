fcfeebdc1da864fd04fc393718e80c0a7dd40ad4
Md.Sakib Hossain <86706077+sakib236160@users.noreply.github.com>
2024-11-04 10:57:24 -0800

    Add or remove element task done

---
+++ Add-or-remove-elements.js
@@ -0,0 +1,14 @@
+//  2. Add or remove elements
+//               Declare an array of 3 tourist destinations
+//               Add a new tourist destination to your tourist array
+//               Add two more to your array
+//               Remove the last tourist destination you have added
+//               display the final array as output
+
+
+const touristPlace = ['France', 'Japan', 'New York City'];
+ touristPlace.push('USA');
+ touristPlace.push('Paris', 'Kyoto');
+ console.log(touristPlace);
+ touristPlace.pop();
+ console.log(touristPlace);
