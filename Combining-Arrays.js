0d0936bac6f9269de2cd727458bb0d4dcd25b516
Md.Sakib Hossain <86706077+sakib236160@users.noreply.github.com>
2024-11-04 11:53:30 -0800

    Combining array done

---
+++ Combining-Arrays.js
@@ -0,0 +1,14 @@
+// 5. Combining Arrays
+//      Instructions:
+//      Create two arrays of your choice.
+//      Use the concat method to combine the two arrays into a new array.
+//      Print both the original arrays and the combined array using console.log().
+
+
+const love = ['I', 'Love'];
+
+const you = ['You', 'Ahlla'];
+
+const loveYou = love.concat(you);
+
+console.log(loveYou);
